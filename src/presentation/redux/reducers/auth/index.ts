b19df10fe50037c099ef5fe0f9ae3ce4bfed7a45
Vo Ta Dung <47474729+votadung2000@users.noreply.github.com>
2025-05-18 09:37:25 +0700

    update logic

--- src/presentation/redux/reducers/auth/index.ts
+++ src/presentation/redux/reducers/auth/index.ts
@@ -1,10 +1,14 @@
 import { authReducer } from './auth.reducer';
-import { authSelector } from './auth.selector';
+import { authSelector, getLoginData, getRegisterData } from './auth.selector';
 import { fetchApiLogin, fetchApiRegister } from './auth.api';
 
 export {
     authReducer,
+
     authSelector,
+    getLoginData,
+    getRegisterData,
+
     fetchApiLogin,
     fetchApiRegister,
 };
